8987d6988e9b2e6e5e63fb8015ada0b7c9c818f2
Andre Kehrer <49730214+andrekehrer@users.noreply.github.com>
2021-09-21 13:49:15 +0100

    Identar codigo

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const port = process.env.PORT || 3000
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const app = express();
+const port = process.env.PORT || 3000;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-app.use('/', (req, res)=>{
-    res.render('index.html');
+app.use("/", (req, res) => {
+  res.render("index.html");
 });
 
 let = msgs = [];
 
-io.on('connection', socket=>{
-    console.log(`socket connectado: ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log(`socket connectado: ${socket.id}`);
 
-    socket.emit('prevMsg', msgs);
+  socket.emit("prevMsg", msgs);
 
-    socket.on('sendMessage', data =>{
-        msgs.push(data);
-        socket.broadcast.emit('receivedMsg',data);
-    });
+  socket.on("sendMessage", (data) => {
+    msgs.push(data);
+    socket.broadcast.emit("receivedMsg", data);
+  });
 });
 
 server.listen(port);
